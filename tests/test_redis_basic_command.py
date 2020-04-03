cdce46fbc4f122193bce5063e1a5b607bcc55e5c
李瑞 <lirui.sep@gmail.com>
2020-04-03 17:22:07 +0800

    feat: remove unit test case that depend on third-part module
    Unit test should not depend on other module, or mock it when logic is
    complex
    (database mq file system ...)

--- tests/test_redis_basic_command.py
+++ tests/test_redis_basic_command.py
@@ -14,11 +14,19 @@ class RedisBasicCommandsTestCase(unittest.TestCase):
         self.assertEqual(200, rv.status_code)
         self.assertEqual("Hello World!", str(rv.data, 'utf-8'))
 
-    def test_echo_command(self):
-        rv = self.app.get('/redis/echo')
-        self.assertIsNotNone(rv)
-        self.assertEqual(200, rv.status_code)
-        self.assertEqual("foo", str(rv.data, 'utf-8'))
+    #   Actually the unit test should test and verify the unit logic,
+    #   Should not depend on third-part module (such as database, file system and so on)
+    #   the demonstration of redis usage is quite sample, do not have too much logic her,
+    #   so It's pretty reasonable to ignore the unittest case.
+    #
+    #   In some complex scenario, use unittest.mock (MagicMock, @patch) to mock the logic.
+    #
+    #
+    # def test_echo_command(self):
+    #     rv = self.app.get('/redis/echo')
+    #     self.assertIsNotNone(rv)
+    #     self.assertEqual(200, rv.status_code)
+    #     self.assertEqual("foo", str(rv.data, 'utf-8'))
 
 
 if __name__ == '__main__':
